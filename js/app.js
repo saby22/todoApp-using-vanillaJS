42cc01e116d393f454bcfd0e26223647f1b6b664
Saby22 <sabyasachi.tffs@gmail.com>
2020-08-19 19:00:41 +0530

    Adding the delete icon slide in feature

--- js/app.js
+++ js/app.js
@@ -1,31 +1,57 @@
-//get all the elements from the DOM
+/*get all the elements from the DOM*/
 const plusButton=document.querySelector(".content__header i")
 const input_div=document.querySelector(".content__add");
 const input=document.querySelector("input");
 const todoDiv=document.querySelectorAll(".content__todos");
-const todoDelete=document.querySelectorAll(".content__todos-delete i");
+const todoDelete=document.querySelectorAll(".content__todos-delete");
 
-//Variables
+/*Variables*/
 let newTodo;
 let toggleNewTodo=true;
+
+/*Functions*/
+
+//to add a class
+const classAdd = (target,className)=>{
+    target.classList.add(className);
+}
+
+//to remove a class
+const classRemove = (target,className)=>{
+    target.classList.remove(className);
+};
+
+//to add style
+const addStyle = (target,cssProperty,value)=>{
+    target.style.cssProperty=value;
+};
+
+//to check if a class exists
+const classExists = (target,className)=>{
+    if(target.classList.contains(className))
+    return true;
+    else
+    return false;
+};
+/*---------------------------------------*/
 
 //Display or hide the Add New Todo
 plusButton.addEventListener("click", ()=>{
     if(toggleNewTodo)
-    input_div.style.display="none";
+    addStyle(input_div,display,"none");
     else
-    input_div.style.display="flex";
+    addStyle(input_div,display,"flex");
     toggleNewTodo=!toggleNewTodo;
 });
 
 //Applying border to 'Add New Todo' when in focus
 input.addEventListener("focus",()=>{
-    input_div.classList.add("content__add-focus");
+    classAdd(input_div,"content__add-focus");
 });
 
 //Removing border to 'Add New Todo' when blurred
 input.addEventListener("blur",()=>{
-    input_div.classList.remove("content__add-focus");
+    classRemove(input_div,"content__add-focus");
 });
 
 //Retrieve value when ENTER key is pressed
@@ -42,14 +68,22 @@ input.addEventListener("keydown",event=>{
 for(let i=0;i<todoDiv.length;i++)
 {
     todoDiv[i].addEventListener("mouseenter",()=>{
-        // todoDelete[i].classList.add("")
-        todoDiv[i].classList.add("content__todos-delete-mouseIn");
-        console.log("Mouse Entered");
+
+        if(classExists(todoDelete[i],"content__todos-delete-mouseOut"))
+        classRemove(todoDelete[i],"content__todos-delete-mouseOut");
+        
+        classAdd(todoDelete[i],"content__todos-delete-mouseIn");
+
+        console.log(`Mouse Entered ${i}`);
     });
 
     todoDiv[i].addEventListener("mouseleave",()=>{
-        todoDiv[i].classList.add("content__todos-delete-mouseOut");
-        console.log("Mouse Left");
+
+        if(classExists(todoDelete[i],"content__todos-delete-mouseIn"))
+        classRemove(todoDelete[i],"content__todos-delete-mouseIn");
+
+        classAdd(todoDelete[i],"content__todos-delete-mouseOut");
+        console.log(`Mouse Left ${i}`);
     });
 }
 
